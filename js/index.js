b15bcffb81030c8570d63f9a9eac89ee27791dcc
GOProtect <51914771+GOProtect@users.noreply.github.com>
2019-06-17 20:50:12 +0800

    0
    0

---
+++ js/index.js
@@ -0,0 +1,599 @@
+card_product = [
+  {
+    "id": "product",
+    "target": "#pro01",
+    "target_id": "pro01",
+    "data": {
+    "title": "酯類全合成機油15W40",
+    "img": "./img/product/15W40/1.jpg"
+  },
+  "data_intro": {
+    "title": "德特15W40四行程全合成酯基引擎機油",
+    "img": "./img/product/15W40/1.jpg",
+    "intro": "4T全合成 15W40\n符合規範:API SN JASO MA-II\n建議售價400\n說明：\n1.以全酯基及PAO全合成基礎油及耐高溫高抗氧化添加劑調配。\n2.含特殊引擎保護配方，強化油膜降低引擎磨損。\n3.低溫容易啟動，全時保護引擎，省油又有力。\n4.特殊高抗剪切黏度向上劑，可避免引擎馬力衰退無力。\n適用：適用各廠牌機車全車系、噴射引擎及陶瓷汽缸引擎。\n包裝：1Liter"
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro02",
+    "target_id": "pro02",
+    "data": {
+    "title": "酯類全合成機油10W50",
+    "img": "./img/product/10W50/1.jpg"
+  },
+  "data_intro": {
+    "title": "德特10W50四行程全合成酯基引擎機油",
+    "img": "./img/product/10W50/1.jpg",
+    "intro": "4T全合成 10W50\n符合規範:API SN JASO MA-II\n建議售價400\n說明：\n1.以全酯基及PAO全合成基礎油及耐高溫高抗氧化添加劑調配。\n2.含特殊引擎保護配方，強化油膜降低引擎磨損。\n3.低溫容易啟動，全時保護引擎，省油又有力。\n4.特殊高抗剪切黏度向上劑，可避免引擎馬力衰退無力。\n適用：適用各廠牌機車全車系、噴射引擎及陶瓷汽缸引擎。\n包裝：1Liter"
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro03",
+    "target_id": "pro03",
+    "data": {
+    "title": "酯類全合成機油10W40",
+    "img": "./img/product/10W40/1.jpg"
+    },
+    "data_intro": {
+    "title": "德特10W40四行程全合成酯基引擎機油",
+    "img": "./img/product/10W40/1.jpg",
+    "intro": "4T全合成 10W40\n符合規範:API SN JASO MA-II\n建議售價400\n說明：\n1.以全酯基及PAO全合成基礎油及耐高溫高抗氧化添加劑調配。\n2.含特殊引擎保護配方，強化油膜降低引擎磨損。\n3.低溫容易啟動，全時保護引擎，省油又有力。\n4.特殊高抗剪切黏度向上劑，可避免引擎馬力衰退無力。\n適用：適用各廠牌機車全車系、噴射引擎及陶瓷汽缸引擎。\n包裝：1Liter"
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro04",
+    "target_id": "pro04",
+    "data": {
+    "title": "酯類全合成機油5W40",
+    "img": "./img/product/5W40/1.png"
+    },
+    "data_intro": {
+    "title": "德特5W40四行程全合成酯基引擎機油",
+    "img": "./img/product/5W40/1.png",
+    "intro": "4T全合成 5W40\n符合規範:API SN JASO MA-II\n建議售價400\n說明：\n1.以全酯基及PAO全合成基礎油及耐高溫高抗氧化添加劑調配。\n2.含特殊引擎保護配方，強化油膜降低引擎磨損。\n3.低溫容易啟動，全時保護引擎，省油又有力。\n4.特殊高抗剪切黏度向上劑，可避免引擎馬力衰退無力。\n適用：適用各廠牌機車全車系、噴射引擎及陶瓷汽缸引擎。\n包裝：1Liter"
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro05",
+    "target_id": "pro05",
+    "data": {
+    "title": "酯類全合成機油5W60",
+    "img": "./img/product/5W60/1.jpg"
+    },
+    "data_intro": {
+    "title": "德特5W40四行程全合成酯基引擎機油",
+    "img": "./img/product/5W60/1.jpg",
+    "intro": "4T全合成 5W60\n符合規範:API SN JASO MA-II\n建議售價400\n說明：\n1.以全酯基及PAO全合成基礎油及耐高溫高抗氧化添加劑調配。\n2.含特殊引擎保護配方，強化油膜降低引擎磨損。\n3.低溫容易啟動，全時保護引擎，省油又有力。\n4.特殊高抗剪切黏度向上劑，可避免引擎馬力衰退無力。\n適用：適用各廠牌機車全車系、噴射引擎及陶瓷汽缸引擎。\n包裝：1Liter"
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro06",
+    "target_id": "pro06",
+    "data": {
+    "title": "即將上市",
+    "img": "./img/product/Coming.jpg"
+    },
+    "data_intro": {
+    "intro_title": "敬請期待",
+    "intro_img":"./img/product/Coming.jpg",
+    "intro": ""
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro07",
+    "target_id": "pro07",
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/Coming.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/product/Coming.jpg",
+      "intro": ""
+  }
+},
+  {
+    "id": "product",
+    "target": "#pro08",
+    "target_id": "pro08",
+    "data": {
+      "title": "即將上市",
+      "img": "./img/product/Coming.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/product/Coming.jpg",
+      "intro": ""
+    }
+  }
+]
+card_experiment = [
+  {
+    "id": "experiment",
+    "target": "#exp01",
+    "target_id": "exp01",
+    "data": {
+      "title": "冷啟動模擬器",
+      "img": "./img/experiment/01-s.jpg"
+    },
+    "data_intro": {
+      "title": "CCS : 冷啟動模擬器 \nASTM : D5293-10",
+      "img":"./img/experiment/Coming.jpg",
+      "intro": "使用冷起動模擬器在-5℃和-35℃之間測定發動機油表觀黏度的標準試驗方法"
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp02",
+    "target_id": "exp02",
+    "data": {
+      "title": "高溫高剪切測定儀",
+      "img": "./img/experiment/02-s.jpg"
+    },
+    "data_intro": {
+      "title": "HTHS : 高溫高剪切測定儀\nASTM : ASTM D-5481-10",
+      "img":"./img/experiment/02.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp03",
+    "target_id": "exp03",
+    "data": {
+      "title": "消泡測試儀",
+      "img": "./img/experiment/03-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM:D892",
+      "img":"./img/experiment/03.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp04",
+    "target_id": "exp04",
+    "data": {
+      "title": "PPD低溫流動測定儀",
+      "img": "./img/experiment/04-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-97",
+      "img":"./img/experiment/04.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp05",
+    "target_id": "exp05",
+    "data": {
+      "title": "MINI-ROTARY VISCOMETER",
+      "img": "./img/experiment/05-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D4684-08",
+      "img":"./img/experiment/05.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp06",
+    "target_id": "exp06",
+    "data": {
+      "title": "自動黏度測定儀",
+      "img": "./img/experiment/06-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-445 VI\nASTM : D-2270",
+      "img":"./img/experiment/06.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp07",
+    "target_id": "exp07",
+    "data": {
+      "title": "閉杯式閃火點測定儀",
+      "img": "./img/experiment/07-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-93",
+      "img":"./img/experiment/07.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp08",
+    "target_id": "exp08",
+    "data": {
+      "title": "開杯式閃火點測定儀",
+      "img": "./img/experiment/08-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-92",
+      "img":"./img/experiment/08.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp09",
+    "target_id": "exp09",
+    "data": {
+      "title": "TAN、TBN 酸價、鹼價測",
+      "img": "./img/experiment/09-s.jpg"
+    },
+    "data_intro": {
+      "title": "TBN ASTM : D-2896\nTAN ASTM : D-664",
+      "img":"./img/experiment/09.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp10",
+    "target_id": "exp10",
+    "data": {
+      "title": "銅板腐蝕測試儀",
+      "img": "./img/experiment/10-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-130",
+      "img":"./img/experiment/10.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp11",
+    "target_id": "exp11",
+    "data": {
+      "title": "色相分析儀",
+      "img": "./img/experiment/11-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-1500",
+      "img":"./img/experiment/11.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp12",
+    "target_id": "exp12",
+    "data": {
+      "title": "831KF Coulometer 庫倫式水分測定",
+      "img": "./img/experiment/12-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-492",
+      "img":"./img/experiment/12.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp13",
+    "target_id": "exp13",
+    "data": {
+      "title": "乳化測定儀",
+      "img": "./img/experiment/13-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D-140",
+      "img":"./img/experiment/13.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp14",
+    "target_id": "exp14",
+    "data": {
+      "title": "傅立葉紅外線光譜儀",
+      "img": "./img/experiment/14-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : E-1421",
+      "img":"./img/experiment/14.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp15",
+      "target_id": "exp15",
+    "data": {
+      "title": "潤滑脂錐入度測定儀",
+      "img": "./img/experiment/15-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : E-1421",
+      "img":"./img/experiment/15.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp16",
+    "target_id": "exp16",
+    "data": {
+      "title": "寬潤滑脂溫度範圍滴定測定儀",
+      "img": "./img/experiment/16-s.jpg"
+    },
+    "data_intro": {
+      "title": "ASTM : D217",
+      "img":"./img/experiment/16.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "experiment",
+    "target": "#exp17",
+    "target_id": "exp17",
+    "data": {
+      "title": "XRF光譜",
+      "img": "./img/experiment/17-s.jpg"
+    },
+    "data_intro": {
+      "title": "XRF光譜儀\nASTM : D7751",
+      "img":"./img/experiment/17.jpg",
+      "intro": ""
+    }
+  }
+]
+card_recommend = [
+  {
+    "id": "recommend",
+    "target": "#rec01",
+    "target_id": "rec01",
+    "data": {
+      "title": "即將上市",
+      "img": "./img/recommend/recommend.jpg"
+    },
+    "data_intro": {
+      "title": "敬請期待",
+      "img": "./img/recommend/recommend.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "recommend",
+    "target": "#rec02",
+    "target_id": "rec02",
+    "data": {
+      "title": "即將上市",
+      "img": "./img/recommend/recommend.jpg"
+    },
+    "data_intro": {
+      "intro_title": "敬請期待",
+      "intro_img":"./img/recommend/recommend.jpg",
+      "intro": ""
+    }
+
+  }
+]
+card_proof = [
+    {
+    "id": "proof",
+    "target": "#pr01",
+    "target_id": "pr01",
+    "data": {
+      "title": "產品投保",
+      "img": "./img/proof/0.jpg"
+    },
+    "data_intro": {
+      "title": "產品投保證明",
+      "img": "./img/proof/0.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "proof",
+    "target": "#pr02",
+    "target_id": "pr02",
+    "data": {
+      "title": "製造商API頁面-1",
+      "img": "./img/proof/API.jpg"
+    },
+    "data_intro": {
+      "title": "製造商API",
+      "img": "./img/proof/API.jpg",
+      "intro": ""
+    }
+
+  },
+  {
+    "id": "proof",
+    "target": "#pr03",
+    "target_id": "pr03",
+    "data": {
+      "title": "製造商API頁面-2",
+      "img": "./img/proof/API-1.jpg"
+    },
+    "data_intro": {
+      "title": "製造商API",
+      "img": "./img/proof/API-1.jpg",
+      "intro": ""
+    }
+  },
+  {
+    "id": "proof",
+    "target": "#pr04",
+    "target_id": "pr04",
+    "data": {
+      "title": "10W40全合成酯基機油性狀表",
+      "img": "./img/proof/P10W40-1.jpg"
+    },
+    "data_intro": {
+      "title": "10W40性狀表",
+      "img": "./img/proof/10W40-1.jpg",
+      "intro": ""
+    }
+
+  }
+]
+//***************************************************//
+//**********************Vue**************************//
+//***************************************************//
+Vue.component("card",{
+  template: "#card",
+  props: ["card_data"]
+})
+
+var vm = new Vue({
+  el: "#app",
+  data: {
+    cards: {
+      "card_product": card_product,
+      "card_experiment": card_experiment,
+      "card_recommend": card_recommend,
+      "card_proof": card_proof
+      // 物件名稱 : 陣列名稱
+      // html呼叫的是物件名稱
+      // 等於是要先跟vue註冊的意思
+      //"card_recommend": [
+      //  ........
+      //]
+    }
+  }
+})
+//***************************************************//
+//*******************functions***********************//
+//***************************************************//
+//載入首頁效果
+window.onload = loading
+function loading(){
+  var home = document.getElementById("home")
+  home.style.opacity = 1
+  $('.cover').ready(function(){
+    $('#loading').css("display","none")
+    $('#main').css("display","block")
+  })
+}
+//點擊continue按鈕後，顯示導覽列、產品、footer
+function start(){
+  $("#nav").css("display","block")
+  $("#product").css("display","block")
+  $("#footer").css("display","block")
+  $("#home").css("display","none")
+}
+//點擊導覽列的首頁名稱，回到首頁，並把其他隱藏
+function home(){
+  $("#home").css("display","block")
+  $("#nav").css("display","none")
+  $("#product").css("display","none")
+  $("#experiment").css("display","none")
+  $("#about").css("display","none")
+  $("#QA").css("display","none")
+  $("#footer").css("display","none")
+}
+//點擊導覽列項目，把其他不相關的區塊隱藏
+function fn(i){
+  $("#footer").css("display","block")
+  $("#home").css("display","none")
+
+  switch(i){
+    case 0:
+      $("#product").css("display","block")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","none")
+      $("#story").css("display","none")
+      break
+    case 1:
+      $("#product").css("display","none")
+      $("#experiment").css("display","block")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","none")
+      $("#story").css("display","none")
+      break
+    case 2:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","block")
+      $("#act").css("display","none")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","none")
+      $("#story").css("display","none")
+      break
+    case 3:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","block")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","none")
+      $("#story").css("display","none")
+      break
+    case 4:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
+      $("#QA").css("display","block")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","none")
+      $("#story").css("display","none")
+      break
+    case 5:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","block")
+      $("#proof").css("display","none")
+      $("#story").css("display","none")
+      break
+    case 6:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","block")
+      $("#story").css("display","none")
+    break
+    case 7:
+      $("#product").css("display","none")
+      $("#experiment").css("display","none")
+      $("#about").css("display","none")
+      $("#act").css("display","none")
+      $("#QA").css("display","none")
+      $("#recommend").css("display","none")
+      $("#proof").css("display","none")
+      $("#story").css("display","block")
+      break
+  }
+}
+//***************************************************//
+//***************************************************//
+//***************************************************//
+//螢幕小於5760px，點擊選單項目後會自動收合(再點一次按鈕)
+$(function(){
+  if($(window).width() < 576){
+    $(".navbar a").on("click",function(){
+      $(".navbar-toggler").click()
+    })
+  }
+})
